d57bcc7ae5c7b5a707781de0995c64ffaba3e60b
Mehrad Rousta <mehrad77@gmail.com>
2018-05-08 19:21:56 +0430

    fix GA events

--- footer.php
+++ footer.php
@@ -18,18 +18,18 @@
 		<footer id="colophon" class="site-footer container clearfix" role="contentinfo">
 			<div class="social">
 			    <ul>
-				<li class="linkedin"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Linkedin','<?php echo wp_get_shortlink(); ?>')" target="_blank" href="https://www.linkedin.com/in/NimaShafiezadeh/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-				<li class="twitter"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Twitter','<?php echo wp_get_shortlink(); ?>')" target="_blank" href="https://www.twitter.com/Retooeter/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-			      <li class="instagram"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Telegram','<?php echo wp_get_shortlink(); ?>')" target="_blank" href="https://www.instagram.com/NimaShafiezadeh/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+				<li class="linkedin"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Linkedin')" target="_blank" href="https://www.linkedin.com/in/NimaShafiezadeh/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+				<li class="twitter"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Twitter'" target="_blank" href="https://www.twitter.com/Retooeter/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+			      <li class="instagram"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Telegram')" target="_blank" href="https://www.instagram.com/NimaShafiezadeh/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
 			    </ul>
 			</div>
               
             
-            <span>از حقوق مادی و معنوی سایتم گذشتم :)</span>
+            <span onclick="ga('send', 'event', 'engagement', 'footer_click', 'copyright')">از حقوق مادی و معنوی سایتم گذشتم :)</span>
 			<div id="footer-text" class="site-info">
 				<?php do_action( 'chista_footer_text' ); ?>
 				<?php //chista_credit_link(); ?>
-				<small class="credit-link">
+				<small class="credit-link" onclick="ga('send', 'event', 'engagement', 'footer_click', 'credit')" >
 		قدرت گرفته از  <a href="https://mehrad.js.org/Chista" title="قالب وردپرس چیستا">چیستا</a> | طراحی با <i class="fa fa-heart" aria-hidden="true"></i> توسط <a href="https://mehrad.js.org" title="مهراد روستا">مهراد روستا</a>
 				</span>
 			</div><!-- .site-info -->
