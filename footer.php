9a8a5ddbfcaed75de29ff05a5ecd53262f2c75e0
Mehrad Rousta @ Raamaan <mehrad77@gmail.com>
2018-09-05 07:11:53 +0430

    remove chista_social_widgets

--- footer.php
+++ footer.php
@@ -16,8 +16,15 @@
 	<div id="footer" class="footer-wrap">
 
 		<footer id="colophon" class="site-footer container clearfix" role="contentinfo">
-			<?php chista_social_widgets() ?>
-              
+			<div class="social">
+				<ul>
+					<li class="linkedin"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Linkedin')" target="_blank" href="<?php echo get_option('linkedin_url') ?>"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+					<li class="twitter"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Twitter'" target="_blank" href="<?php echo get_option('twitter_url') ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li class="instagram"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Instagram')" target="_blank" href="<?php echo get_option('instagram_url') ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li class="slideshare"><a class="transition2s"  onclick="ga('send', 'event', 'engagement', 'social_click', 'Slideshare')" target="_blank" href="<?php echo get_option('slideshare_url') ?>"><i class="fa fa-slideshare" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+				
             
             <span onclick="ga('send', 'event', 'engagement', 'footer_click', 'copyright')"><?php echo get_option('copyright'); ?></span>
 			<div id="footer-text" class="site-info">
@@ -41,6 +48,5 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
